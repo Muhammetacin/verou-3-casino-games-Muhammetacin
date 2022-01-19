fba04ea3607d4b84bc74bf4a46f6e94bedd5d1a0
Muhammet Cin <muhammetacin@gmail.com>
2022-01-19 12:02:07 +0100

    Add todos

---
+++ GuessingGame/js/guessing-game.js
@@ -0,0 +1,6 @@
+// TODO: A secret number will be generated. To make things fair, the number should be between 1 and (learners in the class)
+// TODO: A user can enter a guess (what html element can be useful for this?)
+// TODO: When he or she pushes the "play" button, the number will be compared to the secret number
+// Depending on the outcome, an alert box will show:
+// TODO: Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them.
+// TODO: Bummer.. You guessed (guess) and the secret number was (secret number).
