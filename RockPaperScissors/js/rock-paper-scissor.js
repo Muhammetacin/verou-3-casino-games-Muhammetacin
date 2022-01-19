b1f578b3d5d4937953479bbf0d68263c11f7922c
Muhammet Cin <muhammetacin@gmail.com>
2022-01-19 15:51:36 +0100

    Add different icon pack

--- RockPaperScissors/js/rock-paper-scissor.js
+++ RockPaperScissors/js/rock-paper-scissor.js
@@ -1,14 +1,14 @@
 // VARIABLES: PLAYER AND COMPUTER
-let player, computer, score = 0, gameNumber = 0;
+let player, computer, score = 0, gameNumber = 0, iconChoice = 0;
 const CHOICES = ["Rock", "Paper", "Scissors"];
-const CHOICES2 = ["Plane", "Car", "Horse"];
+const CHOICES2 = ["Plane", "Car", "Bike"];
 
 
 // PLAYER BUTTONS
 const playerButtons = document.querySelector(".buttons");
-const playerRock = playerButtons.children[0];
-const playerPaper = playerButtons.children[1];
-const playerScissors = playerButtons.children[2];
+const playerButton1 = playerButtons.children[0];
+const playerButton2 = playerButtons.children[1];
+const playerButton3 = playerButtons.children[2];
 const playButton = playerButtons.children[3];
 
 for(let playerButton of playerButtons.children) {
@@ -17,19 +17,40 @@ for(let playerButton of playerButtons.children) {
         computerChoice.innerHTML = "";
         resetStyle();
 
-        if(playerButton.innerHTML === "🪨" || playerButton == playerButtons.children[0]) {
-            player = CHOICES[0];
-            selectButtonHighlight(e);
+        if(iconChoice == 0) {
+            if(playerButton == playerButtons.children[0]) {
+                player = CHOICES[0];
+                console.log(player);
+                selectButtonHighlight(e);
+            }
+            if(playerButton == playerButtons.children[1]) {
+                player = CHOICES[1];
+                selectButtonHighlight(e);
+            }
+            if(playerButton == playerButtons.children[2]) {
+                player = CHOICES[2];
+                selectButtonHighlight(e);
+            }
+        } 
+        else if(iconChoice == 1) {
+            if(playerButton == playerButtons.children[0]) {
+                player = CHOICES2[0];
+                console.log(playerButton.innerHTML);
+                selectButtonHighlight(e);
+            }
+            if(playerButton == playerButtons.children[1]) {
+                player = CHOICES2[1];
+                selectButtonHighlight(e);
+            }
+            if(playerButton == playerButtons.children[2]) {
+                player = CHOICES2[2];
+                selectButtonHighlight(e);
+            }
+        } 
+        else if(playerChoice === "") {
+            playButton.disabled = true;
         }
-        if(playerButton.innerHTML === "📃" || playerButton == playerButtons.children[1]) {
-            player = CHOICES[1];
-            selectButtonHighlight(e);
-        }
-        if(playerButton.innerHTML === "✂" || playerButton == playerButtons.children[2]) {
-            player = CHOICES[2];
-            selectButtonHighlight(e);
-        }
-
+        
         playerChoice.innerHTML = "Player chooses '" + player + "'";
     });
 }
@@ -41,7 +62,12 @@ function selectButtonHighlight(button) {
 
 playButton.addEventListener("click", () => {
     resetStyle();
-    computer = CHOICES[Math.floor(Math.random() * 3)];
+    if(iconChoice == 0) {
+        computer = CHOICES[Math.floor(Math.random() * 3)];
+    }
+    else {
+        computer = CHOICES2[Math.floor(Math.random() * 3)];
+    }
     computerChoice.innerHTML = "Computer chooses '" + computer + "'";
     gameNumber++;
     calculateResult();
@@ -118,6 +144,57 @@ function calculateResult() {
             }
             break;
 
+        case "Plane":
+            if(computer === "Plane") {
+                winnerText.innerHTML = "Both player chose " + player + ". It's a draw :/";
+                gameStatusStyle("draw");
+            }
+            if(computer === "Car") {
+                winnerText.innerHTML = "Player wins! " + player + " is faster than " + computer + ".";
+                score++;
+                gameStatusStyle("win");
+            }
+            if(computer === "Bike") {
+                winnerText.innerHTML = "Player loses. Bike is healthy!";
+                // score--;
+                gameStatusStyle("lose");
+            }
+            break;
+
+        case "Car":
+            if(computer === "Bike") {
+                winnerText.innerHTML = "Player wins! " + player + " is faster than " + computer + ".";
+                score++;
+                gameStatusStyle("win");
+            }
+            if(computer === "Plane") {
+                winnerText.innerHTML = "Player loses. " + computer + " has bigger motors than " + player + ".";
+                // score--;
+                gameStatusStyle("lose");
+            }
+            if(computer === "Car") {
+                winnerText.innerHTML = "Both player chose " + player + ". It's a draw :/";
+                gameStatusStyle("draw");
+            }
+            break;
+
+        case "Bike":
+            if(computer === "Car") {
+                winnerText.innerHTML = "Player loses. " + computer + " is faster than " + player + ".";
+                // score--;
+                gameStatusStyle("lose");
+            }
+            else if(computer === "Bike") {
+                winnerText.innerHTML = "Both player chose " + player + ". It's a draw :/";
+                gameStatusStyle("draw");
+            }
+            else if(computer === "Plane") {
+                winnerText.innerHTML = "Player wins! " + player + " is healthier.";
+                score++;
+                gameStatusStyle("win");
+            }
+            break;
+
         default:
             computerChoice.innerHTML = "";
             winnerText.innerHTML = "Player hasn't chosen.";
@@ -153,8 +230,7 @@ function showScore() {
 
 // RESET GAME
 const resetButton = scoreboard.children[1].children[1];
-
-resetButton.addEventListener("click", () => {
+const reset = () => {
     scoreboardValue.innerHTML = "";
     winnerText.innerHTML = "";
     computerChoice.innerHTML = "";
@@ -163,10 +239,46 @@ resetButton.addEventListener("click", () => {
     player = "";
     removeHighlightFromButtons();
     resetStyle();
-});
+};
+
+resetButton.addEventListener("click", reset);
 
 function removeHighlightFromButtons() {
-    playerRock.classList.remove("btnSelected");
-    playerPaper.classList.remove("btnSelected");
-    playerScissors.classList.remove("btnSelected");
+    playerButton1.classList.remove("btnSelected");
+    playerButton2.classList.remove("btnSelected");
+    playerButton3.classList.remove("btnSelected");
 }
+
+
+
+// ChANGE WEAPON
+const changeWeapon = scoreboard.children[1].children[0];
+
+changeWeapon.addEventListener('click', () => {
+    if(iconChoice == 1) {
+        iconChoice = 0;
+        reset();
+        generateIconPack1();
+
+    } else {
+        iconChoice = 1;
+        reset();
+        generateIconPack2();
+    }
+});
+
+const rockPaperScissorsIconPack = ["&#129704;", "&#128195;", "&#9986;"];
+const planeCarBikeIconPack = ["&#9992;", "&#128663;", "&#128690;"];
+
+const generateIconPack1 = () => {
+    playerButton1.innerHTML = rockPaperScissorsIconPack[0];
+    playerButton2.innerHTML = rockPaperScissorsIconPack[1];
+    playerButton3.innerHTML = rockPaperScissorsIconPack[2];
+};
+const generateIconPack2 = () => {
+    playerButton1.innerHTML = planeCarBikeIconPack[0];
+    playerButton2.innerHTML = planeCarBikeIconPack[1];
+    playerButton3.innerHTML = planeCarBikeIconPack[2];
+};
+
+generateIconPack1();
