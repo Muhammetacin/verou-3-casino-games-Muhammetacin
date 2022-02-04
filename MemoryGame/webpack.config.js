eda7a382a34491d524672d98c657b63cff5a57af
Muhammet Cin <muhammetacin@gmail.com>
2022-02-04 12:55:49 +0100

    Config eslint, prettier, webpack + test them

---
+++ MemoryGame/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  mode: "development",
+};
