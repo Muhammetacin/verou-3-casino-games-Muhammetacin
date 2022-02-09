3e7841214176c66f38d7f58a789a02743fce48c0
Muhammet Cin <muhammetacin@gmail.com>
2022-02-09 16:38:07 +0100

    Add github icon in footer

--- MemoryGame/src/index.js
+++ MemoryGame/src/index.js
@@ -1,1 +1,2 @@
 import "./style.scss";
+import "@fortawesome/fontawesome-free/js/brands";
--- MemoryGame/webpack.config.js
+++ MemoryGame/webpack.config.js
@@ -20,6 +20,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
     ],
   },
 };
